36877f05ee4d7e74bb8582330215a5ba697352dd
bathula <bravi@dromos.cl>
2017-09-22 17:05:37 -0300

    updated

---
+++ dispatcher.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+var Queue chan chan WorkRequest
+
+func StartDispatcher(nworkers int) {
+  // First, initialize the channel we are going to but the workers' work channels into.
+  Queue = make(chan chan WorkRequest, nworkers)
+  
+  // Now, create all of our workers.
+  for i := 0; i<nworkers; i++ {
+    fmt.Println("Starting worker", i+1)
+    worker := NewWorker(i+1, Queue)
+    worker.Start()
+  }
+  
+  go func() {
+    for {
+      select {
+      case work := <-WorkQueue:
+        fmt.Println("Received work requeust")
+        go func() {
+          worker := <-Queue
+          
+          fmt.Println("Dispatching work request")
+          worker <- work
+        }()
+      }
+    }
+  }()
+}
